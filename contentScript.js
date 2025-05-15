b6a1fe39189f228ecaa94141019627bfbf229064
Victor Lee <victorleefca@gmail.com>
2025-05-15 01:01:46 -0700

    Added tailwind styling

--- contentScript.js
+++ contentScript.js
@@ -1,20 +1,18 @@
 API_URL = 'https://bnhcsabaaagprnsbiqaj.supabase.co/rest/v1/reports'
 ;(async function extension() {
-    // 1) Extract userId from URL
     const match = location.pathname.match(/^\/users\/(\d+)\/profile/)
     if (!match) return
     const userId = match[1]
 
-    // create container
     const container = document.createElement('div')
     container.id = 'rbx-report-container'
     container.className =
-        'fixed flex top-4 right-4 bg-white shadow-lg rounded-2xl p-4 space-y-3 w-64 z-50'
+        'fixed flex flex-col top-10 right-10 bg-white shadow-lg rounded-sm p-4 space-y-3 w-64 z-50'
     container.innerHTML = `
-    <label class="block text-sm font-medium text-gray-700 underline">
+    <label class="block font-medium text-gray-700 font-bold">
       Report user:
     </label>
-    <div id="rbx-labels" class="space-y-2">
+    <div id="rbx-labels" class="space-y-2 flex flex-col">
       <label class="inline-flex items-center">
         <input type="checkbox" value="Cheating" class="form-checkbox text-indigo-600" />
         <span class="ml-2 text-gray-700">Cheating</span>
